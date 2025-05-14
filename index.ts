c9dfd98e1ec8ca88fa74e6dcde6c9205093d9ad6
v.kragel <v.kragel@softteco.com>
2025-05-14 21:05:36 +0300

    feat: add ability to close websocket

--- index.ts
+++ index.ts
@@ -5,8 +5,29 @@ const HTTP_PORT = 8181;
 const WSS_PORT = 3000;
 
 const httpServer = createHttpServer();
-createWsServer(WSS_PORT);
+const wss = createWsServer(WSS_PORT);
 
 httpServer.listen(HTTP_PORT, () => {
   console.log(`Start static http server on the ${HTTP_PORT} port!`);
 });
+
+function shutdown() {
+  console.log("\nShutting down gracefully...");
+
+  wss.close(() => {
+    console.log("WebSocket server closed");
+  });
+
+  httpServer.close(() => {
+    console.log("HTTP server closed");
+    process.exit(0);
+  });
+
+  setTimeout(() => {
+    console.error("Force exiting...");
+    process.exit(1);
+  }, 5000).unref();
+}
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
