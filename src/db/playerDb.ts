c2025413803e2922781c7533e12610784db5edcb
v.kragel <v.kragel@softteco.com>
2025-05-14 21:05:55 +0300

    refactor: update player db

--- src/db/playerDb.ts
+++ src/db/playerDb.ts
@@ -1,24 +1,10 @@
-type Player = {
-  login: string;
-  password: string;
-};
+import { Player } from "../types/player";
 
 const players: Player[] = [];
 
-export const addPlayer = (login: string, password: string): boolean => {
-  if (players.find((p) => p.login === login)) {
-    return false;
-  }
-
-  players.push({ login, password });
-
-  return true;
-};
-
-export const validatePlayer = (login: string, password: string): boolean => {
-  return players.some((p) => p.login === login && p.password === password);
-};
-
-export const getAllPlayers = (): Player[] => {
-  return players;
+export const playerStore = {
+  getAll: () => players,
+  findByName: (name: string) => players.find((p) => p.name === name),
+  add: (player: Player) => players.push(player),
+  indexOf: (player: Player) => players.indexOf(player),
 };
